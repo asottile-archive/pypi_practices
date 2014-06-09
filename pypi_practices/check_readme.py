52243b49d447fc30dbd229e9360b1ddb79568dde
Anthony Sottile <asottile@umich.edu>
2014-06-08 19:15:48 -0700

    Add a check for an Installation section in readme.

--- pypi_practices/check_readme.py
+++ pypi_practices/check_readme.py
@@ -8,38 +8,36 @@ from pypi_practices.errors import FileValidationError
 from pypi_practices.make_entry import make_entry
 
 
+def _no_key_error(filename, key):
+    return (
+        filename,
+        None,
+        'Could not fix: set `{0}` in .pypi-practices-config.yaml.'.format(
+            key,
+        ),
+    )
+
+
 def _check_file_exists(errors, cwd, fix, config):
+    """Check that the readme exists."""
     readme_path = os.path.join(cwd, 'README.md')
     if os.path.exists(readme_path):
         return
 
     errors.append(('README.md', None, 'File does not exist.'))
 
-    github_user = config.get('github_user', None)
-    package_name = config.get('package_name', None)
-    repo_description = config.get('repo_description', None)
+    github_user = config.get('github_user')
+    package_name = config.get('package_name')
+    repo_description = config.get('repo_description')
 
     if fix and not github_user:
-        errors.append((
-            'README.md',
-            None,
-            'Could not fix: set `github_user` in .pypi-practices-config.yaml.'
-        ))
+        errors.append(_no_key_error('README.md', 'github_user'))
 
     if fix and not package_name:
-        errors.append((
-            'README.md',
-            None,
-            'Could not fix: set `package_name` in .pypi-practices-config.yaml.'
-        ))
+        errors.append(_no_key_error('README.md', 'package_name'))
 
     if fix and not repo_description:
-        errors.append((
-            'README.md',
-            None,
-            'Could not fix: set `repo_description` in '
-            '.pypi-practices-config.yaml.'
-        ))
+        errors.append(_no_key_error('README.md', 'repo_description'))
 
     if fix and github_user and package_name and repo_description:
         errors.append(('README.md', None, 'Fixed!'))
@@ -60,14 +58,72 @@ def _check_file_exists(errors, cwd, fix, config):
     raise FileValidationError(errors)
 
 
+def _check_has_installation(errors, original_contents, fix, config):
+    """Check that the readme has an Installation section."""
+    if '### Installation\n' in original_contents:
+        return original_contents
+
+    errors.append((
+        'README.md', None, 'Expected an ### Installation section.',
+    ))
+
+    package_name = config.get('package_name')
+
+    if fix and not package_name:
+        errors.append(_no_key_error('README.md', 'package_name'))
+
+    if fix and package_name:
+        errors.append(('README.md', None, 'Fixed!'))
+        return original_contents + (
+            '\n'
+            '### Installation\n'
+            '\n'
+            '`$ pip install {0}`\n'
+            '\n\n'.format(package_name)
+        )
+    else:
+        return original_contents
+
+
+# Each step takes the following arguments:
+#    errors - mutable list to append errors to
+#    original_contents - text contents when the step was called
+#    fix - boolean whether the step should fix things
+#    config - pypi_practices config
+# If a step finds a problem it should:
+#    - append to errors
+#    - If `fix` and it is possible: return new updated contents with the fix
+# Otherwise:
+#    - return the original contents and do nothing
+STEPS = [
+    _check_has_installation,
+]
+
+
 def check_readme(cwd, fix, config):
     errors = []
 
+    # First check that our file exists
     _check_file_exists(errors, cwd, fix, config)
 
-    # TODO: attempt to get project name from config
-    # TODO: attempt to get project name from tox.ini
-    # TODO: attempt to get project name from setup.py
+    readme_path = os.path.join(cwd, 'README.md')
+    original_contents = io.open(readme_path).read()
+    contents = original_contents
+
+    # Each step may change the contents / append to errors
+    for step in STEPS:
+        contents = step(errors, contents, fix, config)
+
+    # If a fix modified the contents, write them out
+    if fix and contents != original_contents:
+        with io.open(readme_path, 'w') as readme_file:
+            readme_file.write(contents)
+
+    # If we had errors, raise
+    if errors:
+        raise FileValidationError(errors)
+
+    # Otherwise all is good :)
     return 0
 
 
--- tests/check_readme_test.py
+++ tests/check_readme_test.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 import io
 import os.path
+import pkg_resources
 
+from pypi_practices.check_readme import _check_has_installation
 from pypi_practices.check_readme import check_readme
 from pypi_practices.errors import FileValidationError
 from testing.util import assert_raises_with_msg
@@ -17,8 +19,21 @@ def test_readme_does_not_exist(tmpdir):
 
 
 def test_returns_zero_readme_exists_and_is_correct(tmpdir):
+    """Tests both that when everything is happy we get zero and that our
+    template README file passes all of the checks.
+    """
+    readme_template_file = pkg_resources.resource_filename(
+        'pypi_practices', 'resources/README.md.template',
+    )
+    template_contents = io.open(readme_template_file).read()
     with io.open(os.path.join(tmpdir.strpath, 'README.md'), 'w') as readme:
-        readme.write('my package\n=========\n')
+        readme.write(template_contents.format(
+            github_user='asottile',
+            package_name='pypi_practices',
+            repo_description=(
+                'Scripts enforcing best practices for python packaging'
+            ),
+        ))
 
     ret = check_readme(tmpdir.strpath, False, {})
     assert ret == 0
@@ -38,7 +53,7 @@ def test_cannot_fix_due_to_no_config(tmpdir):
         check_readme(tmpdir.strpath, True, {})
 
 
-def test_fixes(tmpdir):
+def test_fixes_readme_dne(tmpdir):
     with assert_raises_with_msg(
         FileValidationError,
         'README.md: File does not exist.\n'
@@ -63,3 +78,84 @@ def test_fixes(tmpdir):
         'Scripts enforcing best practices for python packaging' in
         readme_contents
     )
+
+
+def test_fixes(tmpdir):
+    readme_path = os.path.join(tmpdir.strpath, 'README.md')
+    with io.open(readme_path, 'w') as readme_file:
+        readme_file.write('before\n')
+
+    with assert_raises_with_msg(
+        FileValidationError,
+        'README.md: Expected an ### Installation section.\n'
+        'README.md: Fixed!'
+    ):
+        check_readme(
+            tmpdir.strpath,
+            True,
+            {'package_name': 'package_name'},
+        )
+
+    new_readme_contents = io.open(readme_path).read()
+    assert new_readme_contents == (
+        'before\n'
+        '\n'
+        '### Installation\n'
+        '\n'
+        '`$ pip install package_name`\n'
+        '\n\n'
+    )
+
+
+def test_installation_has_installation_has_it():
+    contents = '### Installation\n'
+    errors = []
+    ret = _check_has_installation(errors, contents, True, {})
+    assert ret == contents
+    assert errors == []
+
+
+def test_installation_missing_installation_no_fix():
+    contents = 'before\n'
+    errors = []
+    ret = _check_has_installation(errors, contents, False, {})
+    assert ret == contents
+    assert errors == [
+        ('README.md', None, 'Expected an ### Installation section.'),
+    ]
+
+
+def test_installation_fix_no_package_name():
+    contents = 'before\n'
+    errors = []
+    ret = _check_has_installation(errors, contents, True, {})
+    assert ret == contents
+    assert errors == [
+        ('README.md', None, 'Expected an ### Installation section.'),
+        (
+            'README.md',
+            None,
+            'Could not fix: set `package_name` in '
+            '.pypi-practices-config.yaml.'
+        ),
+    ]
+
+
+def test_installation_fixes():
+    contents = 'before\n'
+    errors = []
+    ret = _check_has_installation(
+        errors, contents, True, {'package_name': 'package_name'}
+    )
+    assert ret == (
+        'before\n'
+        '\n'
+        '### Installation\n'
+        '\n'
+        '`$ pip install package_name`\n'
+        '\n\n'
+    )
+    assert errors == [
+        ('README.md', None, 'Expected an ### Installation section.'),
+        ('README.md', None, 'Fixed!'),
+    ]
